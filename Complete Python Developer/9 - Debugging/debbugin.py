c47ce859598c266efec97201843bea897a5f5756
Lukas Rozado <104160624+lukasrozado@users.noreply.github.com>
2022-12-13 05:58:43 -0300

    Add files via upload

---
+++ Complete Python Developer/9 - Debugging/debbugin.py
@@ -0,0 +1,18 @@
+# linting
+#   helps us find bugging codes before happen this is a function of interpreters
+#   num2 = num + 4
+
+# Read Errors
+#   4 + "dajsdahsdja"
+#   name + 4
+
+# pdb python debugger
+
+import pdb
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+print(add(4, 5))
